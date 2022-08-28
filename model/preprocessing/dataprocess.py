bfbb8718b6b38e6b8e8f4f26f2843ef19a6f8042
shen <871520305@qq.com>
2022-08-28 13:30:37 +0800

    test2

--- model/preprocessing/dataprocess.py
+++ model/preprocessing/dataprocess.py
@@ -4,13 +4,13 @@
 # @File : cutimage.py
 
 # from pydde.core.model import BaseProcedureModel
-# import numpy as np
-# from torchvision import transforms
-# from PIL import Image
-# import torchvision.transforms.functional as F
-# import torch
-# import logging
-# from osgeo import gdal
+import numpy as np
+from torchvision import transforms
+from PIL import Image
+import torchvision.transforms.functional as F
+import torch
+import logging
+from osgeo import gdal
 
 
 
